1703eb0af67bd858df621dd9302afcfdffeccbb2
2nytwin <2nytwinconnect@gmail.com>
2023-01-14 20:12:10 +0300

    weather

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+import requests
 
 def index(request):
+    appid = '0cca4103265bb8e736a69753d1f5607a'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' +appid
+
+    city = 'Los Angeles'
+    res = requests.get(url.format(city))
+
+    print(res.text)
+
     return render(request, 'weather/index.html')
