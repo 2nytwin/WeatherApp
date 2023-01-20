a3855dc545ada4ac50ea41ab4b83a04124cd4382
2nytwin <2nytwinconnect@gmail.com>
2023-01-21 00:32:45 +0300

    weather

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,22 +1,36 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
     appid = '0cca4103265bb8e736a69753d1f5607a'
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    city = 'Los Angeles'
-    res = requests.get(url.format(city)).json()
+    if (request.method) == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon']
-    }
+    form = CityForm
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon']
+        }
+
+        all_cities.append(city_info)
 
     context = {
-        'info': city_info
+        'all_info': all_cities,
+        'form': form,
     }
 
     return render(request, 'weather/index.html', context)
